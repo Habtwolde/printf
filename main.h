e408b5a24f6ba59c5ab5e77521fdd95875a802ab
Habtamu Wolde <39864976+Habtwolde@users.noreply.github.com>
2022-11-21 13:46:40 +0300

    update main.h

--- main.h
+++ main.h
@@ -57,5 +57,6 @@ char *fill_oct_array(char *bnr, char *oct);
 char *fill_long_oct_array(char *bnr, char *oct);
 char *fill_short_oct_array(char *bnr, char *oct);
 char *fill_hex_array(char *bnr, char *hex, int isupp, int limit);
+int get_precision(const char *format, int *i, va_list list);
 
 #endif
