5ff71d4a45848137481437488002f6514ea11a5f
Habtamu Wolde <39864976+Habtwolde@users.noreply.github.com>
2022-11-23 23:39:17 +0300

    print-hiphen function is added

--- main.h
+++ main.h
@@ -48,6 +48,7 @@ int prinnoct(va_list arguments, char *buf, unsigned int ibuf);
 int prinnhex(va_list arguments, char *buf, unsigned int ibuf);
 int prinnupx(va_list arguments, char *buf, unsigned int ibuf);
 int prinsint(va_list arguments, char *buf, unsigned int ibuf);
+int print_hiphen(const char *s, int index);
 int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
 int ev_print_func(const char *s, int index);
 unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
